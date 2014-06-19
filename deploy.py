25fb8bc03c4d7dd6fd6e66b826d5d70faeea0481
luo weihua <luo.weihua@gmail.com>
2014-06-19 15:08:33 +0800

    deploy directly

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, re
+os.system('grunt compile')
 os.system('cp -r bin/ /Users/mac/Me/web/sae/houzz/1/static/')
 index = open('bin/index.html', 'r').read()
 temp = re.sub('assets/', '/m/assets/', index)
